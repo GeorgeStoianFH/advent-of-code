14aa1639c987e35b3abe792da67c19723c5e293f
George Stoian <george.stoian@foundationhealth.com>
2025-12-01 11:24:09 +0200

    initialise repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code
+
+Implementations of solutions for the 2025 Advent of Code challenge
---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {prefer: 'type-imports'},
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/prefer-optional-chain': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'no-console': 'off',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', '*.js'],
+  }
+);
